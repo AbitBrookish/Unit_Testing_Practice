105a86191778037913fd4c9640d27cc9d52d2b7f
AbitBrookish <brooksjohnson25@gmail.com>
2023-04-27 11:48:25 -0400

    adds tests

--- test/calc_test.js
+++ test/calc_test.js
@@ -9,10 +9,37 @@ import Calc from "../calc.js";
 describe('Calc', () => {
     describe('.add', () => {
       it('returns the sum of two values', () => {
-        // Your test goes here
         const calc = new Calc();
         const answer = calc.add(1,2);
-        assert.strictEqual(answer, 3);
+        const expected = 1 + 2;
+        assert.strictEqual(answer, expected);
       });
+    });
+
+    describe('.subtract', () => {
+      it('returns the difference of two values', () => {
+        const calc = new Calc();
+        const answer = calc.subtract(17, 7);
+        const expected = 17 - 7;
+        assert.strictEqual(answer, expected);
+      });
+    });
+
+    describe('.multiply', () => {
+      it('returns the product of two values', () => {
+        const calc = new Calc();
+        const answer = calc.multiply(9, 9);
+        const expected = 9 * 9;
+        assert.strictEqual(answer, expected);
+      });
+    });
+
+    describe('.divide', () => {
+      it('returns the quotient of two values', () => {
+        const calc = new Calc();
+        const answer = calc.divide(144, 12);
+        const expected = 144 / 12;
+        assert.strictEqual(answer, expected);
+      })
     });
   });
